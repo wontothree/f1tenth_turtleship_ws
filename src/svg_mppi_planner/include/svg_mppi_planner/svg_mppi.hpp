10b6c17938a28d0d189ae91ea9e60c77d9a0937a
wontothree <prepared7913@naver.com>
2024-11-06 14:36:59 +0000

    add function random sampling

--- src/svg_mppi_planner/include/svg_mppi_planner/svg_mppi.hpp
+++ src/svg_mppi_planner/include/svg_mppi_planner/svg_mppi.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "rclcpp/rclcpp.hpp"
+#include <random>
+#include <omp.h>
+
 #include <grid_map_core/GridMap.hpp>
 
 #include "svg_mppi_planner/common.hpp"
@@ -10,13 +13,27 @@ namespace planning {
 
 class SVGMPPI {
 private:
-    const size_t sample_number_ = 100;
-    const size_t prediction_step_size_ = 15;
+    const size_t sample_number_ = 10;
+    const size_t prediction_step_size_ = 3;
+    const size_t prediction_horizon_ = 3;
     const double prediction_interval_ = 0.05; // s
     const double lf_ = 0.189;
     const double lr_ = 0.135;
     const double collision_weight_ = 1.0;
     const size_t num_svgd_iteration_ = 3;
+    const size_t thread_number_ = 4;
+    const double non_biased_sampling_rate_ = 0.1;
+
+    ControlMeanTrajectory control_mean_trajectory_;
+    ControlCovarianceTrajectory control_covariance_trajectory_;
+    ControlCovarianceTrajectory control_inverse_covariance_trajectory_;
+
+    ControlMeanTrajectoryBatch noise_sample_trajectory_batch_;
+    ControlMeanTrajectoryBatch noised_control_mean_sample_trajectory_batch_;
+
+    // generate random noise
+    std::vector<std::mt19937> random_number_generators_;
+    std::unique_ptr<std::vector<std::array<std::normal_distribution<>, CONTROL_SPACE::dim>>> normal_distribution_pointer_;
 
 public:
 SVGMPPI();
@@ -45,9 +62,37 @@ std::pair<double, double> calculate_state_cost(
     const grid_map::GridMap& local_cost_map
 ) const;
 
+// for stein variational gradient descent
 ControlMeanTrajectoryBatch approximate_gradient_posterior_batch() const;
+ControlMeanTrajectory approximate_gradient_log_likelihood() const;
+
+void random_sampling(
+    const ControlMeanTrajectory& control_mean_trajectory,
+    const ControlCovarianceTrajectory& control_covariance_trajectory
+);
 
 void test();
+
+void set_control_mean_trajectory(
+    const ControlMeanTrajectory& control_mean_trajectory
+)
+{
+    control_mean_trajectory_ = control_mean_trajectory;
+}
+void set_control_covariance_trajectory(
+    const ControlCovarianceTrajectory& control_covariance_trajectory
+)
+{
+    control_covariance_trajectory_ = control_covariance_trajectory;
+
+    // small value to add diagonal elements for preventing singular matrix
+    const double epsilon_ = 1e-4;
+
+    // calculate inverse of covariance matrices in advance to reduce computational cost
+    for (size_t i = 0; i < prediction_step_size_ - 1; i++) {
+        control_inverse_covariance_trajectory_[i] = control_covariance_trajectory_[i].inverse() + epsilon_ * Eigen::MatrixXd::Identity(CONTROL_SPACE::dim, CONTROL_SPACE::dim);
+    }
+}
 
 };
 
--- src/svg_mppi_planner/src/svg_mppi.cpp
+++ src/svg_mppi_planner/src/svg_mppi.cpp
@@ -5,6 +5,40 @@ namespace planning {
 
 SVGMPPI::SVGMPPI() 
 {
+    control_mean_trajectory_ = Eigen::MatrixXd::Zero(
+        prediction_horizon_ - 1, CONTROL_SPACE::dim
+    );
+    control_covariance_trajectory_ = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>(
+        prediction_horizon_ - 1, Eigen::MatrixXd::Zero(CONTROL_SPACE::dim, CONTROL_SPACE::dim)
+    );
+    control_inverse_covariance_trajectory_ = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>(
+        prediction_horizon_ - 1, Eigen::MatrixXd::Zero(CONTROL_SPACE::dim, CONTROL_SPACE::dim)
+    );
+    noise_sample_trajectory_batch_ = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>(
+        sample_number_, Eigen::MatrixXd::Zero(prediction_horizon_ - 1, CONTROL_SPACE::dim)
+    );
+    noised_control_mean_sample_trajectory_batch_ = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>(
+        sample_number_, Eigen::MatrixXd::Zero(prediction_horizon_ - 1, CONTROL_SPACE::dim)
+    );
+
+
+    // initialize random number generator
+    for (int i = 0; i < thread_number_; i++) {
+        random_number_generators_.push_back(std::mt19937(std::random_device{}()));
+    }
+
+    // initialzie normal distributions as standard normal distribution
+    normal_distribution_pointer_ = std::make_unique<std::vector<std::array<std::normal_distribution<>, CONTROL_SPACE::dim>>>();
+    for (size_t i = 0; i < prediction_horizon_ - 1; i++) {
+        std::array<std::normal_distribution<>, CONTROL_SPACE::dim> normal_distributions_ = {};
+
+        for (size_t j = 0; j < CONTROL_SPACE::dim; j++) {
+            std::normal_distribution<> standard_normal_distribution_(0.0, 1.0);
+            normal_distributions_[j] = standard_normal_distribution_;
+        }
+        (*normal_distribution_pointer_).push_back(normal_distributions_);
+    }
+
     test();
 }
 
@@ -133,9 +167,72 @@ std::pair<double, double> SVGMPPI::calculate_state_cost(
 ControlMeanTrajectoryBatch SVGMPPI::approximate_gradient_posterior_batch() const
 {
     ControlMeanTrajectoryBatch gradient_log_posterior_batch_ = std::vector<Eigen::MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd>>(
-        10, Eigen::MatrixXd::Zero(prediction_step_size_, STATE_SPACE::dim) // 10은 임시
+        sample_number_, Eigen::MatrixXd::Zero(prediction_step_size_, STATE_SPACE::dim) // 10은 임시
     );
 
+    // for (size_t i = 0; i < sample_number_; i++) {
+    //     const ControlMeanTrajectory gradient_log_likelihood_ = approximate_gradient_log_likelihood(
+
+    //     )
+    //     gradient_log_posterior_batch_[i] = gradient_log_likelihood_;
+    // }
+
+    return gradient_log_posterior_batch_;
+}
+ControlMeanTrajectory SVGMPPI::approximate_gradient_log_likelihood() const
+{
+    // const ControlCovarianceTrajectory 
+
+    
+}
+
+void SVGMPPI::random_sampling(
+    const ControlMeanTrajectory& control_mean_trajectory,
+    const ControlCovarianceTrajectory& control_covariance_trajectory
+)
+{
+    // set control mean trajectory and control covariance trajectory
+    set_control_mean_trajectory(control_mean_trajectory);
+    set_control_covariance_trajectory(control_covariance_trajectory);
+
+    // set normal distributions parameters
+    for (size_t i = 0; i < prediction_horizon_ - 1; i++) {
+        for (size_t j = 0; j < CONTROL_SPACE::dim; j++) {
+            const double standard_deviation_ = std::sqrt(control_covariance_trajectory_[i](j, j));
+            std::normal_distribution<>::param_type param(0.0, standard_deviation_);
+            (*normal_distribution_pointer_)[i][j].param(param);
+        }
+    }
+
+    #pragma omp parallel for num_threads(thread_number_)
+    for (size_t i = 0; i < sample_number_; i++) {
+        // generate noise sequence
+        for (size_t j = 0; j < prediction_horizon_ - 1; j++) {
+            for (size_t k = 0; k < CONTROL_SPACE::dim; k++) {
+                noise_sample_trajectory_batch_[i](j, k) = (*normal_distribution_pointer_)[j][k](random_number_generators_[omp_get_thread_num()]);
+            }
+        }
+
+        // add noise to mean
+        // sampling control trajectory with non-biased (around zero) sampling rate
+        if (i < static_cast<size_t>((1 - non_biased_sampling_rate_) * sample_number)) {
+            // biased sampling
+            noised_control_mean_sample_trajectory_batch_[i] = control_mean_trajectory_ + noise_sample_trajectory_batch_[i];
+        } else {
+            // non-biased sampling (around zero)
+            noised_control_mean_sample_trajectory_batch_[i] = noise_sample_trajectory_batch_[i];
+        }
+
+        // 여기부터 할 차례 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // clip input with control input constraints
+        for (size_t j = 0; j < CONTROL_SPACE::dim; j++) {
+            for (size_t k = 0; k < prediction_horizon_ - 1; k++) {
+                // noised_control_mean_sample_trajectory_batch_[i](k, j) = std::clamp(noised_control_mean_sample_trajectory_batch_[i](k, j));
+            }
+        }
+    }
 
 
 }
@@ -161,6 +258,69 @@ void SVGMPPI::test()
 
     // std::cout << "Predicted State Trajectory:\n";
     // std::cout << predicted_trajectory << std::endl;
+
+    // -------------------------------------------------------------------------------------------------------------------------------
+
+    // test variable normal_distribution_pointer_
+
+    // 정규 분포의 평균과 표준편차 출력
+    // for (size_t i = 0; i < normal_distribution_pointer_->size(); ++i) {
+    //     std::cout << "정규 분포 집합 " << i + 1 << ":" << std::endl;
+    //     for (size_t j = 0; j < CONTROL_SPACE::dim; ++j) {
+    //         std::cout << "  차원 " << j + 1 << " - 평균: "
+    //                     << (*normal_distribution_pointer_)[i][j].mean()
+    //                     << ", 표준편차: " << (*normal_distribution_pointer_)[i][j].stddev()
+    //                     << std::endl;
+    //     }
+    // }
+
+    // -------------------------------------------------------------------------------------------------------------------------------
+
+    // test function random_sampling
+
+    // Eigen::MatrixXd 형태로 ControlMeanTrajectory 초기화
+    planning::ControlMeanTrajectory control_mean_trajectory(3, 1);  // 3x1 크기 행렬
+    control_mean_trajectory << 0.5, 0.7, 0.9;  // 예시 제어 평균 값 (3x1 행렬)
+
+    // Eigen::MatrixXd 형태로 ControlCovarianceTrajectory 초기화
+    planning::ControlCovarianceTrajectory control_covariance_trajectory;
+    control_covariance_trajectory.push_back(Eigen::MatrixXd::Identity(1, 1) * 0.1);  // 1x1 공분산 행렬
+    control_covariance_trajectory.push_back(Eigen::MatrixXd::Identity(1, 1) * 0.2);  // 1x1 공분산 행렬
+    control_covariance_trajectory.push_back(Eigen::MatrixXd::Identity(1, 1) * 0.3);  // 1x1 공분산 행렬 
+
+
+    // // 제어 평균 출력
+    // std::cout << "Control Mean Trajectory:" << std::endl;
+    // std::cout << control_mean_trajectory << std::endl;
+
+    // // 제어 공분산 출력
+    // std::cout << "\nControl Covariance Trajectory:" << std::endl;
+    // for (size_t i = 0; i < control_covariance_trajectory.size(); ++i) {
+    //     std::cout << "Covariance at time " << i + 1 << ":\n" << control_covariance_trajectory[i] << std::endl;
+    // }
+
+    random_sampling(control_mean_trajectory, control_covariance_trajectory);
+
+    // for (size_t i = 0; i < normal_distribution_pointer_->size(); i++) {
+    //     std::cout << "Normal Distribution Set " << i + 1 << ":" << std::endl;
+    //     for (size_t j = 0; j < CONTROL_SPACE::dim; j++) {
+    //         const auto& dist = (*normal_distribution_pointer_)[i][j];
+    //         std::cout << "  Dimension " << j + 1 << " - Mean: " << dist.mean() 
+    //                   << ", Std Dev: " << dist.stddev() << std::endl;
+    //     }
+    // }
+
+    std::cout << "Noise Sample Trajectory Batch:" << std::endl;
+    for (size_t i = 0; i < sample_number_; i++) {
+        std::cout << "Sample " << i + 1 << ":" << std::endl;
+        for (size_t j = 0; j < prediction_horizon_ - 1; j++) {
+            for (size_t k = 0; k < CONTROL_SPACE::dim; k++) {
+                std::cout << noise_sample_trajectory_batch_[i](j, k) << " ";
+            }
+            std::cout << std::endl;  // 각 행을 출력 후 줄 바꿈
+        }
+        std::cout << "------------------------------" << std::endl;
+    }
 
     // -------------------------------------------------------------------------------------------------------------------------------
 
