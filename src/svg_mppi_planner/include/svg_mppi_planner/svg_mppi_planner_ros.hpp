6b0eae8bdad1a0a84af7a8b148202fd35c033187
wontothree <prepared7913@naver.com>
2024-11-12 08:13:20 +0000

    remark 이진우

--- src/svg_mppi_planner/include/svg_mppi_planner/svg_mppi_planner_ros.hpp
+++ src/svg_mppi_planner/include/svg_mppi_planner/svg_mppi_planner_ros.hpp
@@ -12,31 +12,43 @@ namespace svg_mppi {
 
 class SVGMPPIPlannerROS : public rclcpp::Node
 {
+public:
+    SVGMPPIPlannerROS();
+    ~SVGMPPIPlannerROS() {};
+
 private:
     const double gridLength = 20.0;                     // one side length of sqaure cost map (m)
-    const double resolution = 0.07;                     // resolution of cost map (m/grid)
+    const double resolution = 0.07;                     // resolution of cost map (m / grid)
 
+private:
     std::unique_ptr<svg_mppi::planning::SVGMPPI> svg_mppi_pointer_;
 
     // subscribe
     rclcpp::Subscription<grid_map_msgs::msg::GridMap>::SharedPtr cost_map_subscriber_;
 
+    /**
+    * @brief be called in constant period of timer
+    */
     // timer
     rclcpp::TimerBase::SharedPtr timer_;
-
-    /*
-    * be called in constant period of timer
-    */
     void timer_callback();
 
+    /**
+     * @brief be called whenever be substribed by local cost map
+     */
     bool isLocalCostMapReceived_;
     grid_map::GridMap* local_cost_map_;  
     void local_cost_map_callback(
         const grid_map_msgs::msg::GridMap::SharedPtr local_cost_map
     );
 
-public:
-    SVGMPPIPlannerROS();
-    ~SVGMPPIPlannerROS() {};
+    /**
+     * @brief 이진우
+     */
+    void visualize_state_sequence_batch(
+        // state_sequence_batch
+        // weight_batch
+        // publisher
+    )
 };
 }
